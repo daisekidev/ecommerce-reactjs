aad0dc9fdde73cd8ca90c9d8bbb0fb243166785d
Le Tan Hao <letanhao.personal@gmail.com>
2024-08-11 05:22:52 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Hello from '@components/Hello/Hello';
+
+function App() {
+    return (
+        <>
+            <Hello /> E-Commerce
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,11 @@
+function Button() {
+  return (
+    <div>
+      <button>
+        <div>Click me</div>
+      </button>
+    </div>
+  );
+}
+
+export default Button;
